05fe73c3fe96f16b25511f8da092667f42bbbee4
Stephanie Cheney <steph@scheneyphotography.com>
2020-03-20 21:39:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,8 +6,7 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  hash = {
-   :railroads =>{:pieces => 4, :rent_in_dollars => {}, :names =>{}}
+  hash = {:railroads = {:pieces => 4, :rent_in_dollars => {}, :names =>{}}
 
 end
 
